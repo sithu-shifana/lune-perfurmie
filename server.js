f2fcaf8ec9a91dc2c318aa120ad87744af280a2b
sithu-shifana <sithushifana@gmail.com>
2025-07-28 10:34:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -71,14 +71,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  if (process.env.NODE_ENV === 'production') {
-    if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(`https://${req.headers.host}${req.url}`);
-    }
-  }
-  next();
-});
 
 
 
